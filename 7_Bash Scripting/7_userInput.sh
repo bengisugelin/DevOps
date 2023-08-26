f57e744363c4e47823180d2ffea3b38a38693c8d
Bengi Su Gelin <113550043+bengisugelin@users.noreply.github.com>
2023-08-26 15:38:18 -0700

    7_userInput.sh

---
+++ 7_Bash Scripting/7_userInput.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+echo "Enter you skills:"
+read SKILLS
+
+echo "Your $SKILLS skill is in High demand in the IT indusrtry."
+
+read -p 'Username: ' USR
+read -sp 'Password: ' pass
+
+echo
+
+echo "Login successfull: Welcome USER $USR"
